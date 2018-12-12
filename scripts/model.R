d631f28f46383f4ccb333ff850b800f3d912a21d
Luís Ferreira <luismeruje>
2018-12-12 22:31:55 +0000

    Added first kaggle submission

--- scripts/model.R
+++ scripts/model.R
@@ -48,3 +48,27 @@ gc()
 
 
 # ---------------------------------------------------------------------------- #
+#### Primeira submissão no kagglelibrary(data.table)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+train<-fread("../input/train_V2.csv", header = T)
+train$Id = NULL
+train$groupId = NULL
+train$matchId = NULL
+train$longestKill = NULL
+ff3 <- winPlacePerc ~ . + winPoints * rankPoints
+model <- lm(ff3,data=train)
+
+test<-fread("../input/test_V2.csv", header = T)
+test$Id = NULL
+test$groupId = NULL
+test$matchId = NULL
+test$longestKill = NULL
+p <- predict(model,test) 
+
+test<- fread("../input/test_V2.csv", header = T)
+submission = cbind(Id=test$Id,winPlacePerc=p)
+write.csv(submission,'sample_submission.csv',row.names = FALSE)
+
+
