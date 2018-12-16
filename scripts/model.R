32d99c8170d9f87a17493aef3e1483fd25f02ab4
Luís Ferreira <luismeruje>
2018-12-16 12:24:51 +0000

    Random Forest com parâmetros optimizados

--- scripts/model.R
+++ scripts/model.R
@@ -268,3 +268,97 @@ submission <- cbind(Id=test$Id, winPlacePerc=results)
 #submission<-lapply(submission$winPlacePerc,noOverOne)
 write.csv(submission,'sample_submission.csv',row.names = FALSE)
 
+####################################
+## Random Forests - almost maxed out options
+
+library(data.table)
+library(dplyr)
+library(randomForest)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+train<-fread("../input/train_V2.csv", header = T)
+train <- train %>% 
+  group_by(groupId) %>%
+  summarise(Id = length(Id),
+            assists = sum(assists),
+            DBNOs = sum(DBNOs),
+            boosts = sum(boosts),
+            damageDealt = sum(damageDealt),
+            headshotKills = sum(headshotKills),
+            heals = sum(heals),
+            killPlace= mean(killPlace),
+            killPoints = mean(killPoints),
+            killStreaks = max(killStreaks),
+            kills = sum(kills),
+            longestKill = max(longestKill),
+            matchDuration = mean(matchDuration),
+            rankPoints = mean(rankPoints),
+            revives = sum(revives),
+            rideDistance = max(rideDistance),
+            roadKills = sum(roadKills),
+            swimDistance = max(swimDistance),
+            teamKills = sum(teamKills),
+            vehicleDestroys = sum(vehicleDestroys),
+            walkDistance = max(walkDistance),
+            weaponsAcquired = sum(weaponsAcquired),
+            winPoints = min(winPoints),
+            numGroups = mean(numGroups),
+            maxPlace = mean(maxPlace),
+            winPlacePerc = (mean(winPlacePerc)))
+train$groupId = NULL
+train$longestKill = NULL
+train<-train[!is.na(train$winPlacePerc),]
+y<-train$winPlacePerc
+ff3 <- winPlacePerc ~ .
+train$winPlacePerc=NULL
+model <- randomForest(train,y,nodesize=4,n_jobs=4,maxnodes=2250,data=train)
+summary(model)
+test<-fread("../input/test_V2.csv", header = T)
+test <- test %>% 
+  group_by(groupId) %>%
+  summarise(Id = length(Id),
+            assists = sum(assists),
+            DBNOs = sum(DBNOs),
+            boosts = sum(boosts),
+            damageDealt = sum(damageDealt),
+            headshotKills = sum(headshotKills),
+            heals = sum(heals),
+            killPlace= mean(killPlace),
+            killPoints = mean(killPoints),
+            killStreaks = max(killStreaks),
+            kills = sum(kills),
+            longestKill = max(longestKill),
+            matchDuration = mean(matchDuration),
+            rankPoints = mean(rankPoints),
+            revives = sum(revives),
+            rideDistance = max(rideDistance),
+            roadKills = sum(roadKills),
+            swimDistance = max(swimDistance),
+            teamKills = sum(teamKills),
+            vehicleDestroys = sum(vehicleDestroys),
+            walkDistance = max(walkDistance),
+            weaponsAcquired = sum(weaponsAcquired),
+            winPoints = min(winPoints),
+            numGroups = mean(numGroups),
+            maxPlace = mean(maxPlace))
+groupIdVector <- test$groupId
+test$groupId = NULL
+test$longestKill = NULL
+p <- predict(model,test) 
+
+results <- cbind(groupIdVector,p)
+
+test<- fread("../input/test_V2.csv", header = T)
+ab<-match(test$groupId,results[,1])
+results <- p[ab]
+noUnderZero<-function(x){if(x<0){return (x=0)}else{return(x=x)}}
+noOverOne<-function(x){if(x>1){return (x=1)}else{return(x=x)}}
+#test<- cbind(test,winPlacePerc=results)
+results<- lapply(results, noUnderZero)
+results<- lapply(results,noOverOne)
+results<- unlist(results)
+submission <- cbind(Id=test$Id, winPlacePerc=results)
+#submission[,2]<-lapply(submission[,2],noUnderZero)
+#submission<-lapply(submission$winPlacePerc,noOverOne)
+write.csv(submission,'sample_submission.csv',row.names = FALSE)
